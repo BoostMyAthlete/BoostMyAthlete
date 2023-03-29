a16bd9ed11bf3a5c80d7d107b8922ef34121fa85
Amanda <amandabaratadev@gmail.com>
2023-03-29 16:13:38 +0100

    adding signin feature

---
+++ frontend/src/components/SignupForm/SignupForm.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+
+function SignupForm() {
+  return (
+    <form>
+
+       <label htmlFor="email">E-mail</label> <br />
+        <input placeholder="Insert email: example@example.com" type="email" id="email" /> <br />
+
+
+        <label htmlFor="selector">Profile</label>
+        <br />
+        <select id="selector">
+          <option value="Sponsor">Sponsor</option>
+          <option value="Athlete">Athlete</option>
+        </select>
+
+        <br />
+
+        <label htmlFor="username">Username</label> <br />
+        <input placeholder="Insert your username" type="text" id="username" /> <br />
+
+         <label htmlFor="password">Password</label> <br />
+        <input placeholder="Insert your password" type="password" id="password" /> <br />
+
+      
+        <br />
+  
+        <button type="submit">Submit</button>
+      </form>
+
+  )
+ 
+}
+
+export default SignupForm;
+
+

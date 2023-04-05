fe988f9894af37ea1a4e3388127ee232585ae380
Tomás Botelho Moniz <tbotelhomonizlg@gmail.com>
2023-04-05 14:59:57 +0100

    Submitted the landing content for our project with the right styling

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import StepByStepCards from "@components/Timeline/Timeline";
+import CardList from "@components/LandingContent/LandingContent";
 
 function HomePage() {
   return (
     <div>
+      <CardList />
+    
       <StepByStepCards />
     </div>
   );
